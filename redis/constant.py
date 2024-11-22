a5295f501f72b38dd02e9791285aa6b4f7b28d5a
Dev Sutariya <98572108+devsutariya43@users.noreply.github.com>
2024-11-22 22:36:19 +0530

    initial commit

---
+++ redis/constant.py
@@ -0,0 +1,17 @@
+SENTINEL_HOSTS = [
+    ('localhost', 26379),
+    ('localhost', 26380),
+    ('localhost', 26381)
+]
+
+CREATE_CLUSTER = "./create_cluster.sh"
+
+DEL_CLUSTER = "./del_cluster.sh"
+
+REDIS_HOSTS = "redis_hosts.json"
+
+SERVICE_NAME = "mymaster"
+
+CREATE_SENTINEL = "./create_sentinel.sh"
+
+DEL_SENTINEL = "./del_sentinel.sh"
